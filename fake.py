472859606f443231779a1b42d4d5944bd7822043
jamesmugi <47591280+jamesmugi@users.noreply.github.com>
2021-05-18 11:50:09 +0300

    Update fake.py

--- fake.py
+++ fake.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import itertools
-import sklearn as sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
